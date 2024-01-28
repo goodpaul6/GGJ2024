8d5f96f15e3a0911484ec64e8e38a91440d3932c
Apaar Madan <apaar_madan@hotmail.com>
2024-01-27 21:32:01 -0500

    Update grabbables

--- grabbables.js
+++ grabbables.js
@@ -13,22 +13,39 @@ export function update() {
   const tempVector = new THREE.Vector3();
   const tempMatrix = new THREE.Matrix4();
   const tempQuat = new THREE.Quaternion();
-  for (const gamepad of gamepads) {
-    const pressedValue = 0.4;
-    if (gamepad.buttons[1].value < pressedValue) continue;
-    const controller = gamepad.controller;
-    // Make sure we have the matrix for the controller
-    controller.updateMatrixWorld(true);
 
-    tempMatrix.identity().extractRotation(controller.matrixWorld);
+  for (const grabbable of grabbables) {
+    const worldPos = grabbable.getWorldPosition(new THREE.Vector3());
 
-    tempVector.setFromMatrixPosition(controller.matrixWorld);
-    tempQuat.setFromRotationMatrix(tempMatrix);
+    for (const gamepad of gamepads) {
+      const controller = gamepad.controller;
+      // Make sure we have the matrix for the controller
+      controller.updateMatrixWorld(true);
 
-    for (const grabbable of grabbables) {
-      const worldPos = grabbable.getWorldPosition(new THREE.Vector3());
+      tempMatrix.identity().extractRotation(controller.matrixWorld);
 
-      if (worldPos.distanceTo(tempVector) < 0.2) {
+      tempVector.setFromMatrixPosition(controller.matrixWorld);
+      tempQuat.setFromRotationMatrix(tempMatrix);
+
+      const pressedValue = 0.4;
+
+      if (grabbable.userData.heldByGamepad === gamepad) {
+        if (gamepad.buttons[1].value < pressedValue) {
+          gamepad.hasGrabbedObject = false;
+          grabbable.userData.isGrabbed = false;
+          grabbable.userData.heldByGamepad = null;
+        } else {
+          grabbable.userData.isGrabbed = true;
+          grabbable.userData.grabHandPos = tempVector.clone();
+        }
+      } else if (
+        !gamepad.hasGrabbedObject &&
+        !grabbable.userData.heldByGamepad &&
+        gamepad.buttons[1].value >= pressedValue &&
+        worldPos.distanceTo(tempVector) < 0.2
+      ) {
+        gamepad.hasGrabbedObject = true;
+
         grabbable.userData.isGrabbed = true;
         grabbable.userData.grabHandPos = tempVector.clone();
       }
